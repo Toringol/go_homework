f5b570ae1dc10bce43fe40588555c1e32cdf9587
Toringol <noneoftoringol@gmail.com>
2019-09-20 14:12:16 +0300

    Sth works but not sure about speed  TODO: think about ExecutePipeline and do it

--- homework2/signer.go
+++ homework2/signer.go
@@ -4,37 +4,97 @@ import (
 	"fmt"
 	"sort"
 	"strconv"
+	"sync"
 )
 
-func SingleHash(data int) string {
+func SingleHash(in, out chan interface{}) {
 
-	var strData = strconv.Itoa(data)
+	wg := &sync.WaitGroup{}
 
-	var crt32Data = DataSignerCrc32(strData)
-	var crt32Md5Data = DataSignerCrc32(DataSignerMd5(strData))
+LOOP:
+	for {
+		select {
+		case data := <-in:
+			var strData = strconv.Itoa(data.(int))
+			var crt32Data string
+			var crt32Md5Data string
+			wg.Add(1)
 
-	return crt32Data + "~" + crt32Md5Data
+			go func(data string) {
+				defer wg.Done()
+				crt32Data = DataSignerCrc32(data)
+			}(strData)
+
+			wg.Add(1)
+
+			go func(data string) {
+				defer wg.Done()
+				crt32Md5Data = DataSignerCrc32(DataSignerMd5(data))
+			}(strData)
+
+			wg.Wait()
+			fmt.Println("SingleHash: " + crt32Data + "~" + crt32Md5Data)
+			out <- crt32Data + "~" + crt32Md5Data
+
+		default:
+			break LOOP
+		}
+
+	}
 }
 
-func MultiHash(data int) string {
-	var result string
-	for th := 0; th < 6; th++ {
-		result += DataSignerCrc32(strconv.Itoa(th) + SingleHash(data))
+func MultiHash(in, out chan interface{}) {
+
+	wg := &sync.WaitGroup{}
+
+LOOP:
+	for {
+		select {
+		case data := <-in:
+			var strData = data.(string)
+			var result string
+			var futuresArr [6]string
+
+			for th := 0; th < 6; th++ {
+				wg.Add(1)
+
+				go func(index int, data string) {
+					defer wg.Done()
+					futuresArr[index] = DataSignerCrc32(strconv.Itoa(index) + data)
+					var testIndex string
+					testIndex = strconv.Itoa(index)
+					fmt.Println(data + " " + "MultiHash: " + testIndex + " " + futuresArr[index])
+				}(th, strData)
+			}
+
+			wg.Wait()
+
+			for _, item := range futuresArr {
+				result += item
+			}
+
+			out <- result
+
+		default:
+			break LOOP
+		}
 	}
-	return result
 }
 
-func CombineResults(input []int) string {
-
-	if len(input) == 0 {
-		return ""
-	}
+func CombineResults(in, out chan interface{}) {
 
 	var result string
 
 	var hashArr []string
-	for _, item := range input {
-		hashArr = append(hashArr, MultiHash(item))
+
+LOOP:
+	for {
+		select {
+		case data := <-in:
+			hashArr = append(hashArr, data.(string))
+		default:
+			break LOOP
+		}
 	}
 	sort.Strings(hashArr)
 
@@ -48,11 +108,26 @@ func CombineResults(input []int) string {
 	} else {
 		result = hashArr[0]
 	}
-
-	return result
+	fmt.Println(result)
+	out <- result
 }
 
 func main() {
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	fmt.Println(CombineResults(inputData))
+	chanIn := make(chan interface{}, 2)
+	chanOut := make(chan interface{}, 2)
+
+	chanIn <- 0
+	chanIn <- 1
+
+	SingleHash(chanIn, chanOut)
+
+	chanIn = chanOut
+	chanOut = make(chan interface{}, 2)
+
+	MultiHash(chanIn, chanOut)
+
+	chanIn = chanOut
+	chanOut = make(chan interface{}, 2)
+
+	CombineResults(chanIn, chanOut)
 }
